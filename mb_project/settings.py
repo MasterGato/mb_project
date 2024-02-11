53d006d1f84d0ae2d22b1140bb09c2b9f5527a00
Nermata <Nermata1101@gmail.com>
2024-02-11 23:12:48 +0800

    First Commit

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'posts.apps.PostsConfig',#Tell Django about the new app posts
+    'posts.apps.PostsConfig', #Tell Django about the new app posts
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
